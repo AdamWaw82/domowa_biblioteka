b48cb51af3682231073a1a827ababae02c33947f
Adam Wawerski <adamw467@gmail.com>
2024-09-02 11:43:51 +0200

    Some fixes in start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,23 @@
 @echo off
 
+if not exist "venv" (
+    python -m venv venv
+    echo Środowisko wirtualne utworzone.
+)
+
+call venv\Scripts\activate
+echo Środowisko wirtualne aktywowane.
+
+REM Instalacja zależności
 pip install -r requirements.txt
+
+set FLASK_APP=run.py
+set FLASK_ENV=development
 
 flask db init
 flask db migrate
 flask db upgrade
 
 flask run
+
+deactivate
