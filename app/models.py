194de566e2db50009d75dfe6b3345e65d355d941
Adam Wawerski <adamw467@gmail.com>
2024-09-02 10:49:23 +0200

    Fixed Models relationships... added timezone

--- app/models.py
+++ app/models.py
@@ -1,14 +1,19 @@
 from app import db, datetime
+import pytz
 
 
-# books = db.Table('books_authors', db.Column('book_id', db.Integer, db.ForeignKey('book.id')), db.Column('author_id', db.Integer, db.ForeignKey('author.id')) )
+class BookAuthor(db.Model):
+    __tablename__ = 'book_author'
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
 
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    authors = db.relationship('Author', backref='books', lazy="dynamic")
     is_on_shelf = db.Column(db.Boolean, default=True)
+    authors = db.relationship('Author', secondary='book_author', backref=db.backref('books', lazy=True))
     borrow_records = db.relationship('BorrowRecord', backref='book', lazy=True)
 
 
@@ -18,8 +23,10 @@ class Author(db.Model):
 
 
 class BorrowRecord(db.Model):
+    import pytz
+    print(pytz.all_timezones)
     id = db.Column(db.Integer, primary_key=True)
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
     borrower = db.Column(db.String(120), nullable=False)
-    borrowed_at = db.Column(db.DateTime, default=datetime.utcnow)
+    borrowed_at = db.Column(db.DateTime, default=datetime.now(tz=pytz.timezone('Poland')))
     returned_at = db.Column(db.DateTime, nullable=True)
